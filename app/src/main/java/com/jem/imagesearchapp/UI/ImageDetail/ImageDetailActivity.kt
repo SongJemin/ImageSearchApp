91f95cff1abde66fa6479a2b7bad7ba740541d41
SongJemin <jemin56@naver.com>
2019-08-04 19:36:48 +0900

    Add intentGetData Attach View && Add transparent background in DetailActivity

--- app/src/main/java/com/jem/imagesearchapp/UI/ImageDetail/ImageDetailActivity.kt
+++ app/src/main/java/com/jem/imagesearchapp/UI/ImageDetail/ImageDetailActivity.kt
@@ -1,10 +1,14 @@
 package com.jem.imagesearchapp.UI.ImageDetail
 
+import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
 import com.jem.imagesearchapp.R
+import kotlinx.android.synthetic.main.activity_image_detail.*
 
-class ImageDetailActivity : AppCompatActivity() {
+class ImageDetailActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +19,14 @@ class ImageDetailActivity : AppCompatActivity() {
         var doc_url : String = intent.getStringExtra("doc_url")
         var datetime : String = intent.getStringExtra("datetime")
 
-        
+        var requestManager : RequestManager = Glide.with(this)
+
+        // 받아온 img_url을 이미지 뷰에 씌우기
+        requestManager.load(img_url).into(detail_main_img)
+
+        // 받아온 데이터들 텍스트 뷰에 씌우기
+        detail_title_tv.text = display_sitename
+        detail_doc_tv.text = doc_url
+        detail_date_tv.text = datetime
     }
 }
